720c5fa0926e439bdb18950d8f700e0b51284968
rajatkhanna <rajatkhanna@zooke.net>
2017-08-02 12:08:39 +0530

    Implemented RecyclerView with Grid Layout

--- app/src/main/java/popularmoviesstage1/legalimpurity/com/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/popularmoviesstage1/legalimpurity/com/popularmoviesstage1/MainActivity.java
@@ -1,13 +1,20 @@
 package popularmoviesstage1.legalimpurity.com.popularmoviesstage1;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import popularmoviesstage1.legalimpurity.com.popularmoviesstage1.adapters.MovieListAdapter;
+import popularmoviesstage1.legalimpurity.com.popularmoviesstage1.objects.MovieObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView movie_list_recycler_view;
+    private MovieListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,27 @@ public class MainActivity extends AppCompatActivity {
     private void setView(Activity act)
     {
         movie_list_recycler_view = (RecyclerView) act.findViewById(R.id.movie_list_recycler_view);
+
+        RecyclerView.LayoutManager moviesLayoutManager = new GridLayoutManager(act,2);
+        movie_list_recycler_view.setLayoutManager(moviesLayoutManager);
+
+        movie_list_recycler_view.setHasFixedSize(true);
+
+        ArrayList<MovieObject> movies_list = dummyData();
+        mAdapter = new MovieListAdapter(act,movies_list);
+
+        movie_list_recycler_view.setAdapter(mAdapter);
+
+    }
+
+    private ArrayList<MovieObject> dummyData()
+    {
+        ArrayList<MovieObject> dummyData = new ArrayList<MovieObject>();
+        dummyData.add(new MovieObject("Title","http://i.imgur.com/DvpvklR.png","plot synopsis","userRating",0l));
+        dummyData.add(new MovieObject("Title","http://i.imgur.com/DvpvklR.png","plot synopsis","userRating",0l));
+        dummyData.add(new MovieObject("Title","http://i.imgur.com/DvpvklR.png","plot synopsis","userRating",0l));
+        dummyData.add(new MovieObject("Title","http://i.imgur.com/DvpvklR.png","plot synopsis","userRating",0l));
+        dummyData.add(new MovieObject("Title","http://i.imgur.com/DvpvklR.png","plot synopsis","userRating",0l));
+        return dummyData;
     }
 }
--- app/src/main/java/popularmoviesstage1/legalimpurity/com/popularmoviesstage1/adapters/MovieListAdapter.java
+++ app/src/main/java/popularmoviesstage1/legalimpurity/com/popularmoviesstage1/adapters/MovieListAdapter.java
@@ -1,8 +1,64 @@
 package popularmoviesstage1.legalimpurity.com.popularmoviesstage1.adapters;
 
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import popularmoviesstage1.legalimpurity.com.popularmoviesstage1.R;
+import popularmoviesstage1.legalimpurity.com.popularmoviesstage1.objects.MovieObject;
+
 /**
  * Created by rajatkhanna on 01/08/17.
  */
 
-public class MovieListAdapter {
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MovieItemHolder>{
+
+    private ArrayList<MovieObject> movieObjs;
+    private Activity act;
+
+    public MovieListAdapter(Activity act, ArrayList<MovieObject> movieObjs)
+    {
+        this.movieObjs = movieObjs;
+        this.act = act;
+    }
+
+    @Override
+    public MovieItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View layoutView = LayoutInflater.from(act).inflate(R.layout.movie_list_item, parent,false);
+        MovieItemHolder movieItemHolderObject = new MovieItemHolder(layoutView);
+        return movieItemHolderObject;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieItemHolder holder, int position) {
+        MovieObject mo = movieObjs.get(position);
+        holder.bind(mo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieObjs.size();
+    }
+
+    public class MovieItemHolder extends RecyclerView.ViewHolder{
+
+        public ImageView posterImage;
+
+        public MovieItemHolder(View itemView) {
+            super(itemView);
+            posterImage = (ImageView) itemView.findViewById(R.id.movie_poster);
+        }
+
+        void bind(MovieObject mo)
+        {
+            Picasso.with(act).load(mo.getMoviePosterImageThumbnailUrl()).into(posterImage);
+        }
+    }
 }
